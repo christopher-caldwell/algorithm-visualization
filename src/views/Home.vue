0de2eeafcce019a973012f196e43c88ab6f02f19
caldwell619 <caldwell8814@gmail.com>
2020-02-26 23:49:20 -0600

    working!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,7 @@
 
 <script>
 import { createSource } from '@/util/generateSource'
-import quickSort from '@/util/quickSort'
+// import { swapElements } from '@/util/quickSort'
 export default {
   name: 'Home',
   data(){
@@ -29,21 +29,62 @@ export default {
     }
   },
   methods: {
-    start(){
+    async start(){
       console.log('start')
       const barsCopy = [...this.bars]
-      const sortedBars = quickSort(barsCopy, 0, barsCopy.length - 1, true, this.updateProgress)
+      const sortedBars = await this.quickSort(barsCopy, 0, barsCopy.length - 1, true, this.updateProgress)
       console.log('sorted bars', sortedBars)
-      this.bars = sortedBars
+      // this.bars = sortedBars
       console.log('done')
     },
     updateBars(newNumberOfBars){
       this.numberOfBars = newNumberOfBars
       this.bars = createSource(newNumberOfBars)
     },
-    updateProgress(newBarPositions){
-      this.bars = newBarPositions
-    }
+    waitOn(ms){
+      return new Promise(resolve => setTimeout(resolve, ms))
+    },
+    async swapElements(arr, leftPointer, iterator){
+      // capturing the value of the left pointer
+      let temp = arr[leftPointer];
+      arr[leftPointer]= arr[iterator];
+      arr[iterator] = temp;
+      await this.waitOn(500)
+      this.bars = [...arr]
+      await this.$nextTick()
+    },
+    async partition(arr, left, right, isAscendingOrder){
+      let pivot = arr[left];
+      let  leftPointer = left;
+      // starting at left +1 because left will be the pivot
+      for(let iterator = left + 1; iterator <= right; iterator++){
+        // await waitFor(200)
+        const evaluation = isAscendingOrder
+          ? arr[iterator] < pivot
+          : arr[iterator] > pivot
+        //////////////////////////////////////////////////
+        if (evaluation){
+          // increment the pointer because the proper evaluation has been made
+            leftPointer++;
+            await this.swapElements(arr, leftPointer, iterator)
+        }
+      } 
+      // moving the pivot value
+      // capturing the value at the left pointer
+      const temp = arr[leftPointer];
+      arr[leftPointer] = arr[left];
+      arr[left] = temp;
+
+      return leftPointer;
+    },
+    async quickSort(arr, left, right, isAscendingOrder){
+      if(left < right) {
+        let q = await this.partition(arr, left, right, isAscendingOrder);
+        await this.quickSort(arr, left, q, isAscendingOrder);
+        await this.quickSort(arr, q + 1, right, isAscendingOrder);
+      }
+      return arr
+    },
   },
   mounted(){
     this.bars = createSource(this.numberOfBars)
